5b0abe5ea85e9014d26388f412af8007b040cb14
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-18 14:03:18 +0530

    small

--- src/Pages/SearchResults/SearchResults.jsx
+++ src/Pages/SearchResults/SearchResults.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import{ useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 
