cbe38677bbd379d3c5834ef9a629565737ad641a
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-18 14:54:53 +0530

    name

--- src/Pages/Details/cast/Cast.jsx
+++ src/Pages/Details/cast/Cast.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import "./style.scss";
 
 import ContentWrapper from "../../../Components/contentWrapper/ContentWrapper";
-import Img from "../../../components/lazyLoadImage/Img";
+import Img from "../../../Components/lazyLoadImage/Img";
 import avatar from "../../../assets/avatar.png";
 
 const Cast = ({ data, loading }) => {
